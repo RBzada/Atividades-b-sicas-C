302dc9c2c9be97491de7928f2cac8a0878650d4f
Rafael Barboza <rafal.almeida1096@gmail.com>
2022-04-22 12:47:10 -0300

    Correção de texto

--- Atividades/letrarecorrente.c
+++ Atividades/letrarecorrente.c
@@ -4,16 +4,14 @@ int main(){
     char str;
     int letras[26] = {0};
     int mostcomom = 0;
-    printf("Digite as palavras e aperte enter quando desejar finalizar: ");
+    printf("Digite as palavras em letra maiuscula e aperte enter quando desejar finalizar: ");
     printf("\n");
     while(str != '\n'){
         
         str = getchar();
-
         if(str >= 65 && str <= 90){
             letras[str - 65]++;
-        }
-        
+        } 
     }
 
     for (int i = 0; i < 26; i++){
