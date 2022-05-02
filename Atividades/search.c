47c3ea5525ce6aefc3e2771f0009c427de536335
Rafael Barboza <rafael.almeida1096@gmail.com>
2022-05-02 11:44:18 -0300

    Atividade sobre função search

--- Atividades/search.c
+++ Atividades/search.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+int search(int *arr, int n, int v);
+
 int main(){
-    printf("Hello world");
+    int *p, arr[8] = {1, 2, 3, 5, 10, -8, 7, 1};
+
+    search(arr, 8, 7);
     return 0;
+}
+
+int search(int *arr, int n, int v){
+    int *p, cont = 0;
+    for (p = arr; p < (arr + n); p++){
+        if (*p == v){
+            printf("%d", cont);
+            return 0;
+        }
+        cont += 1;
+    }
+    printf("-1");
 }
