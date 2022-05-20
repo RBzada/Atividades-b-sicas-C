7dcbac1abec7db7a91576531c52649bc44d59ccb
Rafael Barboza <94180312+RBzada@users.noreply.github.com>
2022-05-20 15:55:16 -0300

    Delete Projeto_final directory

--- Projeto_final/limpeza.h
+++
@@ -1,4 +0,0 @@
-#ifndef LIMPEZA_H
-#define LIMPEZA_H
-int clean(int *arr, int n);
-#endif
--- Projeto_final/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "limpeza.h"
-
-int main(){
-    int arr[13] = {5, 3, 3, 8, 7, 10, 10, -5, 2, 7, 12, 9, 10};
-
-    printf("%d", clean(arr, 13));
-
-}
