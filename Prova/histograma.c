4f85b4883b1f3402ab18047b532cca1f031c61f7
Rafael Barboza <rafael.almeida1096@gmail.com>
2022-05-24 11:01:33 -0300

    Adicionando função de criar histograma

---
+++ Prova/histograma.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "histograma.h"
+#include "analisedado.h"
+
+void histograma(int arr[], int n){
+    int *cont = malloc(n * sizeof(int));
+    int max;
+
+    // O array cont recebe a frenquência de todos o números
+    for (int i = 0; i < n; i++){
+        cont[i] = 0;
+    }
+
+    // Contando a frequência de cada número
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (arr[i] == arr[j]){
+                cont[i]++;
+            }
+        }
+    }
+
+    // Colocando 0 nos índices das frequências onde o número em arr[] se repete
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j < n; j++){
+            if (arr[i] == arr[j]){
+                cont[j] = 0;
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j < n; j++){
+            if (arr[i] == arr[j]){
+                arr[j] = 0;
+            }
+        }
+    }
+
+    max = vmax(cont, n);
+    printf("\n\n");
+    for (int i = 0; i < 10 + (max/2); i++){
+        printf(" ");
+    }
+    printf("HISTOGRAMA DAS NOTAS");
+    printf("\n");
+
+    for (int i = 0; i < n; i++){
+        if (arr[i] > 0 || cont[i] != 0){
+            if(arr[i] < 10){
+                printf(" %d |", arr[i]);      
+            }   
+            else{
+                printf("%d |", arr[i]);      
+
+            }
+        }
+        if (arr[i] > 0 || cont[i] != 0){
+            for (int j = 0; j < cont[i]; j++){
+                printf("■   ");
+            }
+            printf("\n");
+        }
+    }
+    printf("▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬");
+    for(int i = 0; i < max; i++){
+        printf(" ▬");
+    }
+    
+    printf("\n  ");
+    for(int i = 1; i <= max; i++){
+        if(i <= 10){
+        printf("  %d ", i);
+        }
+        else{
+            printf(" %d ", i);
+        }
+    }
+
+    printf("\n");
+    for (int i = 0; i < 10 + (max/2); i++){
+        printf(" ");
+    }
+    printf("Frequência");
+
+}
---
+++ Prova/histograma.h
@@ -0,0 +1,8 @@
+#ifndef HISTOGRAMA_H
+#define HISTOGRAMA_H
+
+// Função para plotar histograma
+
+void histograma(int arr[], int n);
+
+#endif
