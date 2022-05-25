1574d0581798b4fcc866ac70828bb0afe730b2c0
Rafael Barboza <rafael.almeida1096@gmail.com>
2022-05-25 20:13:25 -0300

    Organizando

--- Atividades/fill.c
+++ Atividades/fill.c
@@ -11,13 +11,16 @@ int main(){
     for ( int i = 0; i < n; i++){
         printf("%d ", g[i]);
     }
+
     return 0;
 }
 
 int *fill(int n, int v){
-    int *p = malloc(n * sizeof(int));
+    int *p = malloc(n * sizeof(int)); // Não sabemos quanto de espaço é para reservar, então multiplicamos a quantidade de elementos pelo tamanho de 1 int
+
     for (int i = 0; i < n; i++){
         p[i] = v;
     }
+
     return p;
 }
